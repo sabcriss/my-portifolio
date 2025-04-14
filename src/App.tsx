d3a59e583738c5833ee5a13f598f693d3260659b
Sabrina Cristina <163357730+sabcriss@users.noreply.github.com>
2025-04-13 21:56:08 -0300

    add new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Home from "./pages/Home/home"
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Projects from "./pages/Home/sections/Projects/projects"; 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const App = () => {
+  useEffect(() => {
+    // Inicia o AOS
+    AOS.init({ duration: 1000 }); 
+  }, []);
+
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
