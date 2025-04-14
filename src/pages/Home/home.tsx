54c3ddb8e11e92b1beca11baa28b0af8ee35418f
Sabrina Cristina <163357730+sabcriss@users.noreply.github.com>
2025-04-13 21:56:57 -0300

    add

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,16 +1,24 @@
 import NavBar from "../../assets/Images/Components/NavBar/navbar"
 import Hero from "./sections/Hero/hero"
+import Resume from "./sections/Resume/resume"
 import Skills from "./sections/Skills/skills"
+import Projects from "./sections/Projects/projects"
+import Contact from "./sections/Contact/contact"
+import Footer from "./sections/Footer/footer"
 
 
 const Home: React.FC = () => {
   return (
     <>
-    <NavBar/>
+      <NavBar />
       <Hero />
+      <Resume />
       <Skills />
+      <Projects /> 
+      <Contact  />
+      <Footer/>
     </>
   )
 }
 
-export default Home
+export default Home;
