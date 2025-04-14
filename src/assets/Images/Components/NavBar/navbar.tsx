02fc8293b006ac9cca379e186c08e039b71f3727
Sabrina Cristina <163357730+sabcriss@users.noreply.github.com>
2025-04-13 21:55:59 -0300

    new navbar styling

--- src/assets/Images/Components/NavBar/navbar.tsx
+++ src/assets/Images/Components/NavBar/navbar.tsx
@@ -1,25 +1,102 @@
-import { AppBar, MenuItem, Toolbar } from "@mui/material";
+import React, { useState } from "react";
+import { AppBar, Toolbar, IconButton, Typography, Box, MenuItem, Container, Drawer, List, ListItem, ListItemText, useMediaQuery } from "@mui/material";
 import { styled } from "@mui/system";
-import theme from "../../../../theme";
+import { Home, School, Work, Email, Menu as MenuIcon } from "@mui/icons-material";
+import { Link } from "react-scroll";
+
 
 const NavBar: React.FC = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down("sm"));
 
-    const StyledToolbar = styled(Toolbar)(({ theme }) => ({
-        display: "flex",
-        justifyContent: "space-evenly",
-        padding: theme.spacing(1), 
-    }));
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
-    return (
-        <AppBar position="fixed">
-            <StyledToolbar>
-                <MenuItem>About</MenuItem>
-                <MenuItem>Skills</MenuItem>
-                <MenuItem>Projects</MenuItem>
-                <MenuItem>Contact</MenuItem>
-            </StyledToolbar>
-        </AppBar>
-    );
+  const StyledAppBar = styled(AppBar)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.dark,
+    boxShadow: "none",
+  }));
+
+  const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-between",
+    padding: theme.spacing(1, 4),
+  }));
+
+  const drawerList = (
+    <Box sx={{ width: 250 }} onClick={handleDrawerToggle}>
+      <List>
+        <ListItem component={Link} to="about" smooth={true} duration={500}>
+          <Home />
+          <ListItemText primary="About" />
+        </ListItem>
+        <ListItem component={Link} to="skills" smooth={true} duration={500}>
+          <School />
+          <ListItemText primary="Skills" />
+        </ListItem>
+        <ListItem component={Link} to="projects" smooth={true} duration={500}>
+          <Work />
+          <ListItemText primary="Projects" />
+        </ListItem>
+        <ListItem component={Link} to="contact" smooth={true} duration={500}>
+          <Email />
+          <ListItemText primary="Contact" />
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <StyledAppBar position="fixed">
+        <Container>
+          <StyledToolbar>
+            <Typography variant="h6" color="inherit" sx={{ fontWeight: "bold" }}>
+              Portifolio Dev
+            </Typography>
+            {!isMobile && (
+              <Box display="flex" alignItems="center" gap={3}>
+                <MenuItem component={Link} to="about" smooth={true} duration={500}>
+                  <IconButton color="inherit">
+                    <Home />
+                  </IconButton>
+                  About
+                </MenuItem>
+                <MenuItem component={Link} to="skills" smooth={true} duration={500}>
+                  <IconButton color="inherit">
+                    <School />
+                  </IconButton>
+                  Skills
+                </MenuItem>
+                <MenuItem component={Link} to="projects" smooth={true} duration={500}>
+                  <IconButton color="inherit">
+                    <Work />
+                  </IconButton>
+                  Projects
+                </MenuItem>
+                <MenuItem component={Link} to="contact" smooth={true} duration={500}>
+                  <IconButton color="inherit">
+                    <Email />
+                  </IconButton>
+                  Contact
+                </MenuItem>
+              </Box>
+            )}
+            {isMobile && (
+              <IconButton color="inherit" onClick={handleDrawerToggle}>
+                <MenuIcon />
+              </IconButton>
+            )}
+          </StyledToolbar>
+        </Container>
+      </StyledAppBar>
+
+      <Drawer anchor="left" open={drawerOpen} onClose={handleDrawerToggle}>
+        {drawerList}
+      </Drawer>
+    </>
+  );
 };
 
 export default NavBar;
